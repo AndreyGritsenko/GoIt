f148001cf66c13756ded17a3259d1a871ac026b0
AndreyGritsenko <AndreyGritsenko@ya.ru>
2016-04-05 20:00:16 +0300

    1

---
+++ Home_work_js1/js/main.js
@@ -0,0 +1,22 @@
+// alert('Hello word');
+// console.log('hello console');
+var num = prompt('Введите число, которое будем возводить в степень:');
+num = +num;
+var exp = prompt('Введите степень, в которою будем возводить число');
+exp = +exp;
+var i=1;
+while (i < exp){
+  result = result * num;
+  i++;
+}
+console.log(result)
+
+// function power(num, exp) {
+//     var result = 1;
+//     for(var i = 0; i <exp; i++)
+//     {
+//         result = result * num;
+//     }
+//     return(result);
+// }
+// console.log(power(num, exp));
