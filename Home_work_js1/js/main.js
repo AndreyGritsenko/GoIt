78af81cc64ce758012ae40de46b29d104f59974e
AndreyGritsenko <AndreyGritsenko@users.noreply.github.com>
2016-04-08 16:36:27 +0300

    mentor crit

--- Home_work_js1/js/main.js
+++ Home_work_js1/js/main.js
@@ -1,16 +1,8 @@
-// alert('Hello word');
-// console.log('hello console');
 function customPow () {
 var num = prompt('Введите число, которое будем возводить в степень:');
 num = +num;
 var exp = prompt('Введите степень, в которою будем возводить число');
 exp = +exp;
-// var i=1;
-// while (i < exp){
-//   result = result * num;
-//   i++;
-// }
-// console.log(result)
 
 function power(num, exp) {
     var result = 1;
